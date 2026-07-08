d66d7a8053177750090cb68b562a765470400606
Kiste <Mopsdercoole@gmx.at>
2026-07-08 10:54:01 +0200

    feat: add login component and routes, switched to tailwind v3

---
+++ Frontend/src/app/components/login/login.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {SocketService} from '../../services/socket.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class LoginComponent implements OnInit {
+  username: string = '';
+
+  constructor(private socketService: SocketService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.socketService.currentUser$.subscribe(user => {
+      if (user) {
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
+
+  onSubmit(): void {
+    const cleanName = this.username.trim();
+    if (cleanName.length >= 3) {
+      this.socketService.login(cleanName)
+    }
+  }
+
+}
--- Frontend/src/socket.spec.ts
+++ Frontend/src/socket.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SocketService } from './socket.service';
+import { SocketService } from './app/services/socket.service';
 
 describe('Socket', () => {
   let service: SocketService;
